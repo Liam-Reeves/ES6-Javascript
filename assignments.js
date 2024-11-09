469214fa631d8f00620767370b59ead369834e28
unknown <wesleyliam005gmail.com>
2024-11-09 06:24:12 -0800

    first commit

---
+++ assignments.js
@@ -0,0 +1,18 @@
+
+
+const country = "Kenya";
+const continent = "Africa";
+let population = 543000000;
+
+const  isIsland = false;
+let language = "English";
+    console.log(country);
+    console.log(continent);
+    
+    console.log(population);
+    console.log(isIsland);
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+
+// Variables and values
+const firstName = "Jonas";
+    console.log(firstName);
+ 
+ let secondName = "Grace";
+    console.log(secondName);
+
+        let age = 25;
+        console.log(age);
+    
+    let isMarried = true;
+        console.log(isMarried);
+
+    const myFirstjob = "teacher";
+        console.log(myFirstjob);
+        
+        let mySecondJob = "engineer";
+            console.log(mySecondJob);
+
+// Data Types
+    //Primitives
+                    let myAge = 25; // Number or Integer
+                    let myName = "Jonas"; // String 
+
+                    let isSafe = true; // Boolean
+                    let notAllowed = false; // Boolean
+
+                    let myArray = [1, 2, 3, 4, 5]; // Array
+
+                    let gender; //undefined or empty value
+                    let family = null; 
+                        // Symbols
+//BigInt
+
+        // Typeof
+        console.log(typeof myAge); // number
+        console.log(typeof myName); // string
+
+        console.log(typeof isSafe); // boolean
+        console.log(typeof notAllowed); // boolean
+
+        console.log(typeof myArray); // object
+        console.log(typeof gender); // undefined
+        
+        console.log(typeof family); // object
+
+
+
+
